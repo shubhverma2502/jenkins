051cd217600bf97238e3741529f93778e7b36dd7
shubhverma2502 <117101649+shubhverma2502@users.noreply.github.com>
2024-12-20 10:20:00 +0530

    Add files via upload

---
+++ Edvenswa.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class Edvenswa{
+    public static void main(String[] args){
+        System.out.println("Jenkins Learning");
+        System.out.println(new Date());
+    }
+}
